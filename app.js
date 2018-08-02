52b1f5974000939def05415d4ff82a19ac0b10c5
rdeni1993 <denis.ristic@live.co.uk>
2018-08-02 23:20:17 +0200

    I have install modules ==> express, body-parser, path.. Application is initialize and tested

---
+++ app.js
@@ -0,0 +1,12 @@
+// NPM installed modules
+var expressModule = require("express");
+var bodyParserModule = require("body-parser");
+var pathModule = require("path");
+// Instant APP
+var app = expressModule();
+// MiddleWare
+// Set Routes
+// Start app
+console.log("\n\nApplication is running on port 8080");
+app.listen(8080);
+//# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-﻿console.log('Hello world');
+﻿// NPM installed modules
+const expressModule     = require("express");
+const bodyParserModule  = require("body-parser");
+const pathModule        = require("path");
+
+// Instant APP
+const app = expressModule();
+
+// MiddleWare
+
+// Set Routes
+
+// Start app
+console.log("\n\nApplication is running on port 8080");
+app.listen(8080);
